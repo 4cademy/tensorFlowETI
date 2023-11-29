31399da466b4b75a94ca59969c6b408e42902726
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-11-29 16:24:49 +0100

    Reformatted file in Übung 3

--- Uebung3/Post_training_quantization.py
+++ Uebung3/Post_training_quantization.py
@@ -14,43 +14,43 @@ print(tf.version.VERSION)
 train_labels = train_labels
 test_labels = test_labels
 
-
 train_images = train_images.reshape(-1, 28 * 28) / 255.0
 test_images = test_images.reshape(-1, 28 * 28) / 255.0
 
-print(train_images.shape, test_images.shape) # (1000, 784) (1000, 784) 
+print(train_images.shape, test_images.shape)  # (1000, 784) (1000, 784)
 
 
 # Define a simple sequential model
 def create_model():
-  model = tf.keras.Sequential([
-    keras.layers.Dense(512, activation='relu', input_shape=(784,)),
-    #keras.layers.Dropout(0.2),
-    keras.layers.Dense(10)
-  ])
+    model = tf.keras.Sequential([
+        keras.layers.Dense(512, activation='relu', input_shape=(784,)),
+        # keras.layers.Dropout(0.2),
+        keras.layers.Dense(10)
+    ])
 
-  model.compile(optimizer='adam',
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
-  return model
+    return model
+
 
 # to quantize a tensor
-def quantize( values, bits):
-  range = 2**(bits-1)
-  maxVal = tf.reduce_max(tf.abs(values))
-  step = (maxVal / range) 
-  step = tf.pow(2.0, tf.math.ceil(tf.math.log(step)/math.log(2)));
-  qValues = tf.round(values / tf.cast(step,tf.float32))
-  qValues = step * tf.clip_by_value(qValues,-range,range-1)
-  return qValues
+def quantize(values, bits):
+    range = 2 ** (bits - 1)
+    maxVal = tf.reduce_max(tf.abs(values))
+    step = (maxVal / range)
+    step = tf.pow(2.0, tf.math.ceil(tf.math.log(step) / math.log(2)));
+    qValues = tf.round(values / tf.cast(step, tf.float32))
+    qValues = step * tf.clip_by_value(qValues, -range, range - 1)
+    return qValues
+
 
 # Create a basic model instance
 model = create_model()
 
 # Display the model's architecture
 model.summary()
-     
 
 checkpoint_path = "training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -61,8 +61,8 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  verbose=1)
 
 # Train the model with the new callback
-model.fit(train_images, 
-          train_labels,  
+model.fit(train_images,
+          train_labels,
           epochs=5,
           validation_data=(test_images, test_labels),
           callbacks=[cp_callback])  # Pass callback to training
@@ -78,15 +78,13 @@ model = create_model()
 loss, acc = model.evaluate(test_images, test_labels, verbose=2)
 print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
 
-
 # Loads the weights # to turn off the warnings from tensorflow v2, use ---> .expect_partial()
 model.load_weights(checkpoint_path).expect_partial()
 
-#shape of the weights
+# shape of the weights
 print("shape of the weights")
 for w in model.get_weights():
-    print(w.shape) 
-
+    print(w.shape)
 
 '''for layer in model.layers:
   print(layer.name)
@@ -100,13 +98,12 @@ print("Floating point Restored model, accuracy: {:5.2f}%".format(100 * acc))
 
 quant_bits = [1, 2, 4, 6, 8, 16]
 for w_bits in quant_bits:
-  model = create_model()
-  model.load_weights(checkpoint_path).expect_partial()
-  for layer in model.layers:
-    #layer.set_weights([layer.get_weights()[0], layer.get_weights()[1]])
-    layer.set_weights([quantize(layer.get_weights()[0], w_bits), quantize(layer.get_weights()[1], w_bits)])
+    model = create_model()
+    model.load_weights(checkpoint_path).expect_partial()
+    for layer in model.layers:
+        # layer.set_weights([layer.get_weights()[0], layer.get_weights()[1]])
+        layer.set_weights([quantize(layer.get_weights()[0], w_bits), quantize(layer.get_weights()[1], w_bits)])
 
-  # Re-evaluate the model
-  loss, acc = model.evaluate(test_images, test_labels, verbose=2)
-  print(w_bits, "bit quantized restored model, accuracy: {:5.2f}%".format(100 * acc))
-
+    # Re-evaluate the model
+    loss, acc = model.evaluate(test_images, test_labels, verbose=2)
+    print(w_bits, "bit quantized restored model, accuracy: {:5.2f}%".format(100 * acc))
